457ea68b4ebdaa79e15fcdf3f91595af1d5883b3
spinichi <spinichi@naver.com>
2025-11-18 15:46:32 +0900

    fix: AI user 클래스 접근 오류 수정

--- ai/FastAPI/app/services/advice.py
+++ ai/FastAPI/app/services/advice.py
@@ -48,16 +48,16 @@ def connect_weaviate_with_retry(max_retries=5, delay=2):
 client = connect_weaviate_with_retry()
 
 # 유사 상담내용 검색
-async def retrieve_similar_cases(query: str, info: dict, top_k: int = 2):
+async def retrieve_similar_cases(query: str, info, top_k: int = 2):
     try:
         prompt = f"""
         {query}
         사용자 정보
-        나이 : {info["age"]}
-        직업 : {info["job"]}
-        질병력 : {info['disease']}
-        성별 : {info['gender']}
-        거주 형태 : {info['family']}
+        나이 : {info.age}
+        직업 : {info.job}
+        질병력 : {info.disease}
+        성별 : {info.gender}
+        거주 형태 : {info.family}
         """
         # 쿼리 임베딩 생성
         query_vector = embed(prompt)
